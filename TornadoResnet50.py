43bcf0fe0e1b8ca74094af8076845339671b57f7
mcguire1 <mmcguire@towson.edu>
2021-04-13 20:44:30 -0400

    Update TornadoResnet50.py

--- TornadoResnet50.py
+++ TornadoResnet50.py
@@ -35,7 +35,6 @@ from skimage import exposure
 from skimage import segmentation
 
 def ResNet(nc_file_path, tornado_file_path, output_path):
-    
     # Read netCDF files from NARR data (example shown for CAPE).
     DS_mf = xr.open_mfdataset(nc_file_path,chunks={'time':5})
     min_x = 64926
@@ -135,70 +134,51 @@ def convolutional_block(X, f, filters, stage, block, s = 2):
     # defining name basis
     conv_name_base = 'res' + str(stage) + block + '_branch'
     bn_name_base = 'bn' + str(stage) + block + '_branch'
-    
     #Retrieve Filters
     F1, F2, F3 = filters
-    
     #Save the input value
     X_shortcut = X
-
-
     #Main Path
     #First component of main path 
     X = Conv2D(F1, (1, 1), strides = (s,s), name = conv_name_base + '2a', kernel_initializer = glorot_uniform(seed=0))(X)
     X = BatchNormalization(axis = 3, name = bn_name_base + '2a')(X)
     X = Activation('relu')(X)
-
     #Second component of main path (≈3 lines)
     X = Conv2D(filters = F2, kernel_size = (f, f), strides = (1,1), padding = 'same', name = conv_name_base + '2b', kernel_initializer = glorot_uniform(seed=0))(X)
     X = BatchNormalization(axis = 3, name = bn_name_base + '2b')(X)
     X = Activation('relu')(X)
-
-
     #Third component of main path (≈2 lines)
     X = Conv2D(filters = F3, kernel_size = (1, 1), strides = (1,1), padding = 'valid', name = conv_name_base + '2c', kernel_initializer = glorot_uniform(seed=0))(X)
     X = BatchNormalization(axis = 3, name = bn_name_base + '2c')(X)
-
-
     #Shortcut Path
     X_shortcut = Conv2D(filters = F3, kernel_size = (1, 1), strides = (s,s), padding = 'valid', name = conv_name_base + '1',
                         kernel_initializer = glorot_uniform(seed=0))(X_shortcut)
     X_shortcut = BatchNormalization(axis = 3, name = bn_name_base + '1')(X_shortcut)
-
     #Final step: Add shortcut value to main path, and pass it through a RELU activation (≈2 lines)
     X = Add()([X, X_shortcut])
     X = Activation('relu')(X)
-    
-    
     return X
 
 #Define Resnet 50 model
 def ResNet50(input_shape=(245, 307, 1), classes=4):
     # Define the input as a tensor with shape input_shape
     X_input = Input(input_shape)
-
     # Zero-Padding
     X = ZeroPadding2D((3, 3))(X_input)
-
     # Stage 1
     X = Conv2D(64, (7, 7), strides=(2, 2), name='conv1', kernel_initializer=glorot_uniform(seed=0))(X)
     X = BatchNormalization(axis=3, name='bn_conv1')(X)
     X = Activation('relu')(X)
     X = MaxPooling2D((3, 3), strides=(2, 2))(X)
-
     # Stage 2
     X = convolutional_block(X, f=3, filters=[64, 64, 256], stage=2, block='a', s=1)
     X = identity_block(X, 3, [64, 64, 256], stage=2, block='b')
     X = identity_block(X, 3, [64, 64, 256], stage=2, block='c')
-
-    ### START CODE HERE ###
-
     # Stage 3 (≈4 lines)
     X = convolutional_block(X, f = 3, filters = [128, 128, 512], stage = 3, block='a', s = 2)
     X = identity_block(X, 3, [128, 128, 512], stage=3, block='b')
     X = identity_block(X, 3, [128, 128, 512], stage=3, block='c')
     X = identity_block(X, 3, [128, 128, 512], stage=3, block='d')
-
     # Stage 4 (≈6 lines)
     X = convolutional_block(X, f = 3, filters = [256, 256, 1024], stage = 4, block='a', s = 2)
     X = identity_block(X, 3, [256, 256, 1024], stage=4, block='b')
@@ -206,22 +186,17 @@ def ResNet50(input_shape=(245, 307, 1), classes=4):
     X = identity_block(X, 3, [256, 256, 1024], stage=4, block='d')
     X = identity_block(X, 3, [256, 256, 1024], stage=4, block='e')
     X = identity_block(X, 3, [256, 256, 1024], stage=4, block='f')
-
     # Stage 5 (≈3 lines)
     X = convolutional_block(X, f = 3, filters = [512, 512, 2048], stage = 5, block='a', s = 2)
     X = identity_block(X, 3, [512, 512, 2048], stage=5, block='b')
     X = identity_block(X, 3, [512, 512, 2048], stage=5, block='c')
-
     # AVGPOOL (≈1 line). Use "X = AveragePooling2D(...)(X)"
     X = AveragePooling2D((2,2), name="avg_pool")(X)
-
     # output layer
     X = Flatten()(X)
     X = Dense(classes, activation='softmax', name='fc' + str(classes), kernel_initializer = glorot_uniform(seed=0))(X)
-    
     # Create model
     model = Model(inputs = X_input, outputs = X, name='ResNet50')
-
     return model
 
 if __name__ == "__main__":
